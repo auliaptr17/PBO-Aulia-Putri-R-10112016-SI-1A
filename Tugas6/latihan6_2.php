1e7068a93e66d0cbb7c14aadfa79d2df2b3404dc
auliaptr17 <auliaputrir1706@gmail.com>
2026-03-14 11:03:42 +0700

    Add files via upload

---
+++ Tugas6/latihan6_2.php
@@ -0,0 +1,9 @@
+<?php
+
+//variabel array
+$buah= ["Apel", "Alpukat", "Mangga"];
+
+foreach ($buah as $b){
+    echo $b . "<br>";
+
+}
---
+++ Tugas6/latihan6_4.php
@@ -0,0 +1,11 @@
+<?php
+
+$i = 1;
+
+while($i <= 5){
+    echo "Perulangan ke-".$i."<br>";
+
+    $i++;
+}
+
+?>
